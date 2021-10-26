dfa8be2eefc392b5cf925c32b0dd39fb4ac0710a
SasambaBitch <sukhovey.vlad@gmail.com>
2021-10-26 17:56:16 +1000

    initial commit

--- matrix.py
+++ matrix.py
@@ -1,10 +1,21 @@
-import numpy as np
-
-
-A = np.array([[5, 7, 1], [9, 0, 1], [1, 6, 0]])
-
-B = np.array([[3, 11, 15], [0, 5, 7], [1, 2, 8]])
-
-
-Symma = A + B
-print(Symma)
+A = [[1,2,3], 
+     [4,5,6], 
+     [7 ,8,9]] 
+  
+B = [[9,8,7], 
+     [6,5,4], 
+     [3,2,1]] 
+  
+  
+result = [[0,0,0], 
+          [0,0,0], 
+          [0,0,0]] 
+  
+# iterate through rows 
+for i in range(len(A)):    
+# iterate through columns 
+    for j in range(len(A[0])): 
+       [i][j] + B[i][j] 
+  
+for x in result: 
+    print(x)
